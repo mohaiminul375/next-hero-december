4a53e3052b125cee34d47fca4c5f6baac43919de
Mohaiminul375 <mohaiminul375@gmail.com>
2024-12-26 14:02:05 +0600

    create loading and error

---
+++ src/app/loading.jsx
@@ -0,0 +1,11 @@
+"use client";
+
+const Loading = () => {
+  return (
+    <div className="min-h-screen">
+      <h2 className="text-teal-500 text-center">Loading..........</h2>
+    </div>
+  );
+};
+
+export default Loading;
---
+++ src/app/not-found.jsx
@@ -0,0 +1,8 @@
+"use client";
+import React from "react";
+
+const error = () => {
+  return <div>from error page</div>;
+};
+
+export default error;
